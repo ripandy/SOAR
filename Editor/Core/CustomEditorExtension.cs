0d012e6f8b1e18dc33574a58f38fecca9e74af5e
Ripandy Adha <ripandy.adha@gmail.com>
2024-08-11 05:55:53 +0900

    feat: custom drawer for Soar's core classes

---
+++ Editor/Core/CustomEditorExtension.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Soar
+{
+    public static class CustomEditorExtension
+    {
+        public static IEnumerable<SerializedProperty> GetChildren(this SerializedProperty property)
+        {
+            property = property.Copy();
+            var nextElement = property.Copy();
+            if (!nextElement.NextVisible(false))
+            {
+                nextElement = null;
+            }
+
+            if (!property.NextVisible(true))
+            {
+                yield break;
+            }
+
+            do
+            {
+                if (SerializedProperty.EqualContents(property, nextElement))
+                {
+                    yield break;
+                }
+
+                yield return property;
+            } while (property.NextVisible(false));
+        }
+    }
+}
