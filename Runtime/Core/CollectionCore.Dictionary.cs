3868e200540adaf6e03631ed222308f95125d0b0
Ripandy Adha <ripandy.adha@gmail.com>
2024-08-25 05:23:19 +0900

    feat: add Value Objects

--- Runtime/Core/CollectionCore.Dictionary.cs
+++ Runtime/Core/CollectionCore.Dictionary.cs
@@ -230,20 +230,4 @@ namespace Soar.Collections
         private partial void RaiseValue(TKey key, TValue value);
         private partial void ClearValueSubscriptions();
     }
-
-    [Serializable]
-    public struct SerializedKeyValuePair<TKey, TValue>
-    {
-        [field:SerializeField] public TKey Key { get; internal set; }
-        [field:SerializeField] public TValue Value { get; internal set; }
-    
-        public SerializedKeyValuePair(TKey key, TValue value)
-        {
-            Key = key;
-            Value = value;
-        }
-        
-        public static implicit operator KeyValuePair<TKey, TValue>(SerializedKeyValuePair<TKey, TValue> serializedKeyValuePair) => new(serializedKeyValuePair.Key, serializedKeyValuePair.Value);
-        public static implicit operator SerializedKeyValuePair<TKey, TValue>(KeyValuePair<TKey, TValue> keyValuePair) => new(keyValuePair.Key, keyValuePair.Value);
-    }
 }
