4d62a41d2f7540232ad3f79840d35ef32dc7a4b4
Ripandy Adha <ripandy.adha@gmail.com>
2025-08-17 06:24:07 +0900

    fix: StringUnityEventBinder to properly listen to events

--- Runtime/Base/UnityEventBinder/StringUnityEventBinder.cs
+++ Runtime/Base/UnityEventBinder/StringUnityEventBinder.cs
@@ -4,6 +4,10 @@ namespace Soar.Events
     {
         protected override void Start()
         {
+            base.Start();
+            // NOTE: GameEvent<string> should be listened to by the base class.
+            //       Other than that raise the event with the string representation of the GameEvent.
+            if (gameEventToListen is GameEvent<string>) return;
             subscriptions.Add(gameEventToListen.Subscribe(() => onTypedGameEventRaised.Invoke(gameEventToListen.ToString())));
         }
     }
