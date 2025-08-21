27117f92febd7b671a91b23015be208f1924b614
Ripandy Adha <ripandy.adha@gmail.com>
2025-08-21 16:49:52 +0900

    chore: remove logs, simplify subscription, update value on subscribe

--- Samples~/CollectionSamples/Scripts/CollectionViewHandler.cs
+++ Samples~/CollectionSamples/Scripts/CollectionViewHandler.cs
@@ -10,7 +10,7 @@ namespace Soar.Collections.Sample
         [SerializeField] private IntList intCollection;
 
         private readonly List<TMP_Text> texts = new();
-        private CompositeDisposable subscriptions = new();
+        private readonly CompositeDisposable subscriptions = new();
 
         private void Awake()
         {
@@ -26,13 +26,9 @@ namespace Soar.Collections.Sample
                 
                 if (!isVisible) continue;
                 
-                Debug.Log($"[{GetType().Name}] Subscribing to index {i} with value {intCollection[i]}");
                 var idx = i;
-                intCollection.SubscribeToValues(idx, value =>
-                {
-                    Debug.Log($"[{GetType().Name}] Value at index {idx} changed to {value}");
-                    texts[idx].text = value.ToString();
-                }).AddTo(subscriptions);
+                texts[idx].text = intCollection[idx].ToString();
+                intCollection.SubscribeToValues(idx, value => texts[idx].text = value.ToString()).AddTo(subscriptions);
             }
 
             intCollection.SubscribeOnAdd(OnCollectionAdded).AddTo(subscriptions);
@@ -43,26 +39,19 @@ namespace Soar.Collections.Sample
         {
             var idx = intCollection.Count - 1;
             texts[idx].transform.parent.gameObject.SetActive(true);
-            Debug.Log($"[{GetType().Name}] Value at index {idx} added with value {addedValue}. Subscribing to index {idx} with value {intCollection[idx]}");
-            intCollection.SubscribeToValues(idx, value =>
-            {
-                Debug.Log($"[{GetType().Name}] Value at index {idx} changed to {value}");
-                texts[idx].text = value.ToString();
-            }).AddTo(subscriptions);
+            texts[idx].text = addedValue.ToString();
+            intCollection.SubscribeToValues(idx, value => texts[idx].text = value.ToString()).AddTo(subscriptions);
         }
         
         private void OnCollectionRemoved(int removedValue)
         {
             var idx = intCollection.Count;
             texts[idx].transform.parent.gameObject.SetActive(false);
-            Debug.Log($"[{GetType().Name}] Value at index {idx} removed.");
         }
 
         private void OnDestroy()
         {
             subscriptions?.Dispose();
-            subscriptions = null;
-            texts.Clear();
         }
     }
 }
